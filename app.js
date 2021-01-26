af4c4af90b5738d7abfbcef3d442d4ecfccbd9c2
alvarezlucass <76540489+alvarezlucass@users.noreply.github.com>
2021-01-26 17:38:14 -0300

    Organizando

---
+++ app.js
@@ -0,0 +1,10 @@
+const express =require("express");
+const app= express();   
+const path =require("path");
+const publicPath=path.resolve("public");
+app.listen(3000, ()=> console.log("Servidor prueba 3000"));
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+});
