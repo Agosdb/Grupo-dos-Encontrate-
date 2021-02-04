a7316c0dbfd5116c5095de82dadf162b6fd71835
alvarezlucass <alvarezlucass@hotmail.com>
2021-02-04 14:42:54 -0300

    actu

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express =require("express");
+
 const app= express();   
+
 const path =require("path");
+
 const publicPath=path.resolve("public");
-app.listen(3030, ()=> console.log("Servidor prueba 3030"));
+
 app.use(express.static(publicPath));
 
+app.listen(3030, ()=> {
+    console.log("Servidor prueba 3030")
+});
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./views/home.html"));
+    res.sendFile(path.resolve("./views/index.html"));
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"))
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/productCar.html"))
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/productDetail.html"))
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"))
 });
