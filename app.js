427955a98b705772c787c6e6bb6a30aa12824bf0
alvarezlucass <alvarezlucass@hotmail.com>
2021-02-11 02:37:40 -0300

    punto

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ app.listen(3030, ()=> {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "./views/index.html")
+    res.sendFile(__dirname + "/views/index.html")
 });
 // app.post('/', (req,res)=>{
 //     res.send(req.body);
 // });
 app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "./views/login.html")
+    res.sendFile(__dirname + "/views/login.html")
 });
 app.get("/productCar", (req, res) => {
-    res.sendFile(__dirname + "./views/productCar.html")
+    res.sendFile(__dirname + "/views/productCar.html")
 });
 app.get("/productDetail", (req, res) => {
-    res.sendFile(__dirname +"./views/productDetail.html")
+    res.sendFile(__dirname +"/views/productDetail.html")
 })
 app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "./views/register.html")
+    res.sendFile(__dirname + "/views/register.html")
 });
