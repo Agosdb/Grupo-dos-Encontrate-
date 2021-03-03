af9b522d241043d5983fdb83b15b65a3507f4c8b
Agostina di Bello <agosdibello@hotmail.com>
2021-03-02 21:37:06 -0300

    Solucionado el tema de EJS - funcionando

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express =require("express");
 const app= express();   
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let mainRoutes = require("./Routes/mainRoutes");
 
 app.listen(3030, ()=> {
     console.log("Servidor prueba 3030")
 });
 
+app.use("/", mainRoutes);
+
+/*/
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/index.ejs")
 });
 app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/views/login.html")
+    res.sendFile(__dirname + "/views/login.ejs")
 });
 app.get("/productCar", (req, res) => {
-    res.sendFile(__dirname + "/views/productCar.html")
+    res.sendFile(__dirname + "/views/productCar.ejs")
 });
 app.get("/productDetail", (req, res) => {
-    res.sendFile(__dirname +"/views/productDetail.html")
+    res.sendFile(__dirname +"/views/productDetail.ejs")
 })
 app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "/views/register.html")
+    res.sendFile(__dirname + "/views/register.ejs")
 });
 app.get("/registerAdministrator", (req, res) => {
-    res.sendFile(__dirname + "/views/registerAdministrator.html")
+    res.sendFile(__dirname + "/views/registerAdministrator.ejs")
 });
 app.get("/productEdition", (req, res) => {
-    res.sendFile(__dirname + "/views/productEdition.html")
-});
-
+    res.sendFile(__dirname + "/views/productEdition.ejs")
+});*/
---
+++ controllers/mainController.js
@@ -0,0 +1,14 @@
+const controller = {
+    index: (req, res) => {
+    return res.render("index"); //entre comillas el nombre de lo que queremos mostrar//cambie el send 
+    //por el render para renderizar la vista//
+    },
+    login: (req, res) => {
+        return res.render("login");
+        },
+    register: (req, res) => {
+       return res.render("register");
+        },
+    }
+    
+    module.exports = controller;
