b0b08ae445968f561a8301b27dd2f025ad09bbe4
alvarezlucass <alvarezlucass@hotmail.com>
2021-03-29 03:42:12 -0300

    para corregir

--- middlerwares/validateRegisterMiddleware.js
+++ middlerwares/validateRegisterMiddleware.js
@@ -2,16 +2,8 @@
 const path = require('path');
 const { body } = require('express-validator');
 
-
-
-module.exports = [
-    body('userName').notEmpty().withMessage('Selecciona tu nombre de usuario'),
-    body('first_name').notEmpty().withMessage('Completa tu nombre'),
-    body('last_name').notEmpty().withMessage('Completa tu apellido'),
-    body('email').notEmpty().withMessage('Completa tu mail'),
-    body('password').notEmpty().withMessage('Completa tu contraseña'),
-    body('category').notEmpty().withMessage('Selecciona tu categoría'),
-        let file = req.file;
+function  validations (req, res, next) {
+    let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.gif'];
 
         if (!file) {
@@ -19,7 +11,37 @@ module.exports = [
         } else {
             let fileExtension = path.extname(file.originalname);
             if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(',')`};
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(',')}`);
             }
+            next();
         }
-];
+}
+module.exports = validations;
+
+
+
+
+
+
+
+
+// function validations = [
+    // body('userName').notEmpty().withMessage('Selecciona tu nombre de usuario'),
+    // body('first_name').notEmpty().withMessage('Completa tu nombre'),
+    // body('last_name').notEmpty().withMessage('Completa tu apellido'),
+    // body('email').notEmpty().withMessage('Completa tu mail'),
+    // body('password').notEmpty().withMessage('Completa tu contraseña'),
+    // body('category').notEmpty().withMessage('Selecciona tu categoría'),
+//         let file = req.file;
+//         let acceptedExtensions = ['.jpg', '.png', '.gif'];
+
+//         if (!file) {
+//             throw new Error('Debes subir una imagen');            
+//         } else {
+//             let fileExtension = path.extname(file.originalname);
+//             if (!acceptedExtensions.includes(fileExtension)) {
+//                 throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(',')`};
+//             }
+//         }
+// ];
+// module.exports = validations;
